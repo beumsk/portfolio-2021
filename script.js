9d24d9941cde8e837f8a6effceb6381b2fbd1846
Rémy Beumier <beumieremy@gmail.com>
2017-02-03 14:30:22 +0100

    hamburger menu totally responsive

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
+if ($(window).width() < 768) {
+	// hide cross and menu
+	$("#menu").hide();
+	$("#cross").hide();
+	$("#menu").removeClass("hidden");
+	$("#cross").removeClass("hidden");
 
+	// show hamburger menu
+	$("#burger").on("click", function() {
+		$("#menu").slideToggle(function() {
+			$("#burger").hide();
+			$("#cross").show();
+		});
 
-$("#burger").on("click", function() {
-	$("#menu").slideToggle(function() {
-		$("#burger").hide();
-		$("#cross").show();
 	});
-	
-});
 
-$("#cross").on("click", function() {
-	$("#menu").slideToggle(function() {
-		$("#cross").hide();
-		$("#burger").show();
+	// hide burger menu
+	$("#cross").on("click", function() {
+		$("#menu").slideToggle(function() {
+			$("#cross").hide();
+			$("#burger").show();
+		});
 	});
-});
+	// hide burger menu
+	$("#menu").on("click", function() {
+		$("#menu").slideToggle(function() {
+			$("#cross").hide();
+			$("#burger").show();
+		});
+	});
+}
