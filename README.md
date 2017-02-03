288f59489373ec3fc512c572dd0450ae4b5b7702
Rémy Beumier <beumieremy@gmail.com>
2017-02-03 12:35:44 +0100

    responsive menu

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This repository is meant for the creation of my website.
 
 It will be a portfolio site built with HTML, CSS & JS mainly.
 
-Colors -> #00796B #009688 #CFD8DC #FFC107
+Colors -> #00796B #009688 #B2DFDB #CFD8DC #FFC107
 Sections -> home > skills > creations > contact
 
 ToDo:
--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+
+
 $("#burger").on("click", function() {
-	$(this).toggleClass("show");
+	$("#menu").slideToggle(function() {
+		$("#burger").hide();
+		$("#cross").show();
+	});
+	
+});
+
+$("#cross").on("click", function() {
+	$("#menu").slideToggle(function() {
+		$("#cross").hide();
+		$("#burger").show();
+	});
 });
