c7673e19b386442ba6979d366a1d728bed51e9f9
Rémy Beumier <beumieremy@gmail.com>
2017-02-06 16:24:21 +0100

    Current section highlighted added

--- script.js
+++ script.js
@@ -39,7 +39,32 @@ $("a[href^='#']").on("click", function() {
 	return false;
 });
 
+
 // highlight current section
+$(document).on("scroll", function() {
+	if ($(document).scrollTop() < $("#about").offset().top) {
+		$(".intLinks").css("background", "#009688");
+	}
+	else if ($(document).scrollTop() < $("#skills").offset().top && $(document).scrollTop() >= $("#about").offset().top) {
+		$(".intLinks").css("background", "#009688");
+		$("[href='#about']").css("background", "#00796B");
+	}
+	else if ($(document).scrollTop() < $("#creations").offset().top && $(document).scrollTop() >= $("#skills").offset().top) {
+		$(".intLinks").css("background", "#009688");
+		$("[href='#skills']").css("background", "#00796B");
+	}
+	else if ($(document).scrollTop() < $("#contact").offset().top && $(document).scrollTop() >= $("#creations").offset().top) {
+		$(".intLinks").css("background", "#009688");
+		$("[href='#creations']").css("background", "#00796B");
+	}
+	else if ($(document).scrollTop() < $("footer").offset().top && $(document).scrollTop() >= $("#contact").offset().top) {
+		$(".intLinks").css("background", "#009688");
+		$("[href='#contact']").css("background", "#00796B");
+	}
+});
 
 
-
+// highlight on hover does not work after scroll !!!
+$(".intLinks").hover(function() {
+	(this).css("background", "#00796B");
+});
