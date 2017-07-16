d9aa06101c3e386545fcecec3a7dce60b91bd58e
Beumsk <beumieremy@gmail.com>
2017-07-16 12:48:42 +0200

    burger menu improved

--- script.js
+++ script.js
@@ -15,14 +15,14 @@ $("#cross").on("click", function() {
 
 // Manage scroll
 $("a[href^='#']").on("click", function() {
-	if ($(window).width() < 748) {
-		$("#menu").hide();
-		$("#cross").hide();
-		$("#burger").show();
-	}
-	var theId = $(this).attr("href");
-	$("html, body").animate({scrollTop:($(theId).offset().top)-50}, "slow");
-	return false;
+  if ($(window).width() < 748) {
+    $("#menu").addClass("hidden");
+    $("#cross").addClass("hidden");
+    $("#burger").removeClass("hidden");
+  }
+  var theId = $(this).attr("href");
+  $("html, body").animate({scrollTop:($(theId).offset().top)-50}, "slow");
+  return false;
 });
 
 
@@ -30,26 +30,26 @@ $("a[href^='#']").on("click", function() {
 $(".load1, .load2, .load3, .load4").hide();
 var load = 0;
 $("#more > span").on("click", function () {
-	load++;
-	switch (load) {
-		case 1:
-			$(".load1").fadeIn(1000, "linear");
-			break;
-		case 2:
-			$(".load2").fadeIn(1000, "linear");
-			$("#more").html("<a href='http://codepen.io/beumsk/pens/public/' target='_blank'><span>More creations on Codepen.io</span></a>");
-			$("#more a").css("text-decoration", "none");
-			break;
-		
-			
-	}
+  load++;
+  switch (load) {
+    case 1:
+      $(".load1").fadeIn(1000, "linear");
+      break;
+    case 2:
+      $(".load2").fadeIn(1000, "linear");
+      $("#more").html("<a href='http://codepen.io/beumsk/pens/public/' target='_blank'><span>More creations on Codepen.io</span></a>");
+      $("#more a").css("text-decoration", "none");
+      break;
+    
+      
+  }
 });
 
 
 // Send email
 var myform = $("form#myform");
 myform.submit(function(event){
-	event.preventDefault();
+  event.preventDefault();
 
   // Change to your service ID, or keep using the default service
   var service_id = "default_service";
@@ -57,8 +57,8 @@ myform.submit(function(event){
 
   $("#button").attr("value", "Sending...");
   emailjs.sendForm(service_id,template_id,"myform")
-  	.then(function(){ 
-    	alert("Thank you for your message !");
+    .then(function(){ 
+      alert("Thank you for your message !");
        $("#button").attr("value", "Send");
     }, function(err) {
        alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
@@ -73,25 +73,25 @@ myform.submit(function(event){
 // highlight current section
 // highlight on hover does not work after scroll !!!
 /*$(document).on("scroll", function() {
-	if ($(document).scrollTop() < $("#about").offset().top) {
-		$(".intLinks").css("background", "#009688");
-	}
-	else if ($(document).scrollTop() < $("#skills").offset().top && $(document).scrollTop() >= $("#about").offset().top) {
-		$(".intLinks").css("background", "#009688");
-		$("[href='#about']").css("background", "#00796B");
-	}
-	else if ($(document).scrollTop() < $("#creations").offset().top && $(document).scrollTop() >= $("#skills").offset().top) {
-		$(".intLinks").css("background", "#009688");
-		$("[href='#skills']").css("background", "#00796B");
-	}
-	else if ($(document).scrollTop() < $("#contact").offset().top && $(document).scrollTop() >= $("#creations").offset().top) {
-		$(".intLinks").css("background", "#009688");
-		$("[href='#creations']").css("background", "#00796B");
-	}
-	else if ($(document).scrollTop() < $("footer").offset().top && $(document).scrollTop() >= $("#contact").offset().top) {
-		$(".intLinks").css("background", "#009688");
-		$("[href='#contact']").css("background", "#00796B");
-	}
+  if ($(document).scrollTop() < $("#about").offset().top) {
+    $(".intLinks").css("background", "#009688");
+  }
+  else if ($(document).scrollTop() < $("#skills").offset().top && $(document).scrollTop() >= $("#about").offset().top) {
+    $(".intLinks").css("background", "#009688");
+    $("[href='#about']").css("background", "#00796B");
+  }
+  else if ($(document).scrollTop() < $("#creations").offset().top && $(document).scrollTop() >= $("#skills").offset().top) {
+    $(".intLinks").css("background", "#009688");
+    $("[href='#skills']").css("background", "#00796B");
+  }
+  else if ($(document).scrollTop() < $("#contact").offset().top && $(document).scrollTop() >= $("#creations").offset().top) {
+    $(".intLinks").css("background", "#009688");
+    $("[href='#creations']").css("background", "#00796B");
+  }
+  else if ($(document).scrollTop() < $("footer").offset().top && $(document).scrollTop() >= $("#contact").offset().top) {
+    $(".intLinks").css("background", "#009688");
+    $("[href='#contact']").css("background", "#00796B");
+  }
 });*/
 
 
