15002b88870af5115b86ff743054bf738cd432ef
Beumier, Rémy <beumieremy@gmail.com>
2019-02-21 15:40:21 +0100

    creation fadeIn in pure JS

--- script.js
+++ script.js
@@ -51,16 +51,18 @@ var section = document.querySelectorAll("section");
 var sections = {};
 
 window.onscroll = function() {
-  Array.prototype.forEach.call(section, function(e) {
-    sections[e.id] = e.offsetTop - 50;
-  });
-  var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-  for (var i in sections) {
-    if (sections[i] <= scrollPosition) {
-      if (document.querySelector('.active')) {
-        document.querySelector('.active').classList.remove("active");
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    Array.prototype.forEach.call(section, function(e) {
+      sections[e.id] = e.offsetTop - 50;
+    });
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    for (var i in sections) {
+      if (sections[i] <= scrollPosition) {
+        if (document.querySelector('.active')) {
+          document.querySelector('.active').classList.remove("active");
+        }
+        document.querySelector('a[href*=' + i + ']').classList.add("active");
       }
-      document.querySelector('a[href*=' + i + ']').classList.add("active");
     }
   }
 };
@@ -68,40 +70,39 @@ window.onscroll = function() {
 
 
 // Manage .more creation button (button transforms into codepen link when all loaded)
-var $load1 = $(".load-1");
-var $load2 = $(".load-2");
-var $more = $(".more > button");
-var load = 0;
-
-$more.on("click", function () {
-  load++;
-  switch (load) {
-    case 1:
-      $load1.fadeIn(1000, "linear");
-      break;
-    case 2:
-      $load2.fadeIn(1000, "linear");
-      $more.parent().html("<a href='https://codepen.io/beumsk/pens/public/' target='_blank' rel='noreferrer noopener' style='text-decoration: none;'>More creations on Codepen</a>");
-      break;
-  }
-});
-
-// smoother way to make creation appear
-// var load1 = document.querySelectorAll(".load-1");
-// var load2 = document.querySelectorAll(".load-2");
-// var moreButton = document.querySelector(".more > button");
+// var $load1 = $(".load-1");
+// var $load2 = $(".load-2");
+// var $more = $(".more > button");
 // var load = 0;
 
-// moreButton.addEventListener("click", function() {
+// $more.on("click", function () {
 //   load++;
-//   if (load === 1) {
-//     for (var j=0; j<load1.length; j++) { load1[j].classList.remove("hidden"); }
-//   }
-//   else if (load === 2) {
-//     for (var k=0; k<load2.length; k++) { load2[k].classList.remove("hidden"); }
-//     moreButton.parentNode.innerHTML = "<a href='https://codepen.io/beumsk/pens/public/' target='_blank' rel='noreferrer noopener' style='text-decoration: none;'>More creations on Codepen</a>";
+//   switch (load) {
+//     case 1:
+//       $load1.fadeIn(1000, "linear");
+//       break;
+//     case 2:
+//       $load2.fadeIn(1000, "linear");
+//       $more.parent().html("<a href='https://codepen.io/beumsk/pens/public/' target='_blank' rel='noreferrer noopener' style='text-decoration: none;'>More creations on Codepen</a>");
+//       break;
 //   }
 // });
+
+var load1 = document.querySelectorAll(".load-1");
+var load2 = document.querySelectorAll(".load-2");
+var moreButton = document.querySelector(".more > button");
+var load = 0;
+
+moreButton.addEventListener("click", function() {
+  load++;
+  if (load === 1) {
+    for (var j=0; j<load1.length; j++) { load1[j].classList.remove("hidden"); }
+  }
+  else if (load === 2) {
+    for (var k=0; k<load2.length; k++) { load2[k].classList.remove("hidden"); }
+    moreButton.parentNode.innerHTML = "<a href='https://codepen.io/beumsk/pens/public/' target='_blank' rel='noreferrer noopener' style='text-decoration: none;'>More creations on Codepen</a>";
+  }
+});
 
 
 
