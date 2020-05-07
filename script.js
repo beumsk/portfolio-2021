82a6840679d1e84f50f9be46a5044b648bd79293
Rémy Beumier <beumieremy@gmail.com>
2020-05-07 09:17:09 +0200

    on scroll animation added

--- script.js
+++ script.js
@@ -24,6 +24,30 @@ window.onscroll = function() {
 
 
 
+// Manage on scroll animations through AOS script
+AOS.init({
+  // Global settings:
+  disable: function() { var maxWidth = 768; return window.innerWidth < maxWidth; }, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 240, // offset (in px) from the original trigger point
+  delay: 100, // values from 0 to 3000, with step 50ms
+  duration: 800, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: true, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
+
+
+
 // Manage .more creation button (button transforms into codepen link when all loaded)
 var load1 = document.querySelectorAll(".load-1");
 var load2 = document.querySelectorAll(".load-2");
